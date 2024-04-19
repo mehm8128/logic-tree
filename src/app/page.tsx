c5be082cf668163ce208f64c1899c8871cc004dd
mehm8128 <mehm8128@gmail.com>
2024-04-19 16:45:57 +0900

    編集・削除機能つけた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,8 +31,41 @@ function TreeBranch({
 	setTree
 }: { tree: Tree; setTree: Dispatch<SetStateAction<Tree>> }) {
 	const [showCurrentLevelBranch, setShowCurrentLevelBranch] = useState(false)
+	const [openContextMenuId, setOpenContextMenuId] = useState<string | null>(
+		null
+	)
+	const [editingValue, setEditingValue] = useState<string>('')
 	const addCurrentLevelBranch = () => {
 		setShowCurrentLevelBranch(!showCurrentLevelBranch)
+	}
+
+	const handleOpenContextMenu = (
+		e: React.MouseEvent<HTMLButtonElement>,
+		id: string
+	) => {
+		e.preventDefault()
+		setOpenContextMenuId(id)
+	}
+	const handleEdit = (id: string) => {
+		setTree(prev => {
+			const newTree = structuredClone(prev)
+			const tree = searchTree(newTree, id)
+			if (!tree) return prev
+			tree.word = editingValue
+			return { ...newTree }
+		})
+		setEditingValue('')
+		setOpenContextMenuId(null)
+	}
+	const handleDelete = (id: string) => {
+		setTree(prev => {
+			const newTree = structuredClone(prev)
+			const parent = searchTree(newTree, tree.id, true)
+			if (!parent) return prev
+			parent.children = parent.children.filter(child => child.id !== id)
+			return { ...newTree }
+		})
+		setOpenContextMenuId(null)
 	}
 
 	const handleSetTree = (newTree: SetStateAction<Tree>) => {
@@ -41,7 +74,11 @@ function TreeBranch({
 	}
 
 	return (
-		<div className={styles.branchRoot}>
+		<div
+			className={styles.branchRoot}
+			onClick={() => setOpenContextMenuId(null)}
+			onKeyDown={() => {}}
+		>
 			{tree.children.length === 0 && (
 				<div className={styles.firstInput}>
 					<span>----</span>
@@ -49,27 +86,33 @@ function TreeBranch({
 				</div>
 			)}
 			{tree.children.map((child, i) => (
-				<>
+				<div key={child.id}>
 					{i !== 0 && <div className={styles.divider}>|</div>}
-					<div
-						key={child.id}
-						className={styles.branchChild}
-						data-notfirst={i !== 0}
-					>
+					<div className={styles.branchChild} data-notfirst={i !== 0}>
 						--
 						{i === 0 && '--'}
 						<div className={styles.wordButtonContainer}>
 							<button
 								onClick={addCurrentLevelBranch}
+								onContextMenu={e => handleOpenContextMenu(e, child.id)}
 								type="button"
 								className={styles.wordButton}
 							>
 								{child.word}
 							</button>
+							{openContextMenuId === child.id && (
+								<ContextMenu
+									editingValue={editingValue}
+									setEditingValue={setEditingValue}
+									handleEdit={handleEdit}
+									handleDelete={handleDelete}
+									child={child}
+								/>
+							)}
 						</div>
 						<TreeBranch tree={child} setTree={setTree} />
 					</div>
-				</>
+				</div>
 			))}
 			{showCurrentLevelBranch && (
 				<div className={styles.branchLastChild}>
@@ -84,8 +127,56 @@ function TreeBranch({
 	)
 }
 
-const searchTree = (tree: Tree, id: string): Tree | null => {
-	if (tree.id === id) return tree
+function ContextMenu({
+	editingValue,
+	setEditingValue,
+	handleEdit,
+	handleDelete,
+	child
+}: {
+	editingValue: string
+	setEditingValue: Dispatch<SetStateAction<string>>
+	handleEdit: (id: string) => void
+	handleDelete: (id: string) => void
+	child: Tree
+}) {
+	return (
+		<div
+			className={styles.contextMenu}
+			onClick={e => e.stopPropagation()}
+			onKeyDown={() => {}}
+		>
+			<div className={styles.editContainer}>
+				<input
+					onChange={e => setEditingValue(e.target.value)}
+					value={editingValue}
+					className={styles.input}
+				/>
+				<button
+					onClick={() => handleEdit(child.id)}
+					type="button"
+					className={styles.button}
+				>
+					確定
+				</button>
+			</div>
+			<button
+				onClick={() => handleDelete(child.id)}
+				type="button"
+				className={styles.button}
+			>
+				削除
+			</button>
+		</div>
+	)
+}
+
+const searchTree = (tree: Tree, id: string, parent = false): Tree | null => {
+	if (parent) {
+		if (tree.children.some(child => child.id === id)) return tree
+	} else {
+		if (tree.id === id) return tree
+	}
 	for (const child of tree.children) {
 		const result = searchTree(child, id)
 		if (result) return result
@@ -130,7 +221,7 @@ function AddWord({
 			<button
 				onClick={() => settleWord(value)}
 				type="button"
-				className={styles.addButton}
+				className={styles.button}
 			>
 				Add
 			</button>
